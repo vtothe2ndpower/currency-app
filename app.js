3ecd504b71874b3f1bd911fd0431876145e5b5a7
Victor Vargas <vvargasnunez@gmail.com>
2021-07-17 17:19:11 -0700

    Added currency conversion functionality

--- app.js
+++ app.js
@@ -1,63 +1,63 @@
+// Check for what happens on 1-2-3-4-3-2-1 character string count
+// CHange the title
+// Make responsive
+
 // Variables
 const userInput = document.getElementById('userInput');
+const practiceButton = document.getElementById('convertButton');
+let currencyData;
 
 userInput.addEventListener('keyup', (e) => {
   const target = e.target;
   const searchQuery = target.value;
-  console.log(searchQuery);
-
-  if (searchQuery.length < 3) {
-    document.getElementById('root').innerHTML = '';
-
-    return false;
-  }
 
   gatherMatchedCountries(searchQuery);
 
-  // after 2 or more chars 
-  // make request
-
-    // Hide and Show accordingly
+// Hide and Show accordingly - instead of making a ton of requests
 });
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  gatherAllCountries();
-})
-
 // Functions
-// This gathers our data from the 3rd party API
+// This gathers our country data from the 3rd party API on page load
 const gatherAllCountries = () => {
   fetch('https://restcountries.eu/rest/v2/all')
   .then(res => res.json())
   .then(data => {
-    console.log(data);
 
+      console.log(data);
+      generateDropdown(data);
   })
   .catch(err => console.log('Error: ', err));
 }
 
+const gatherCurrencyData = () => {
+    fetch(`http://data.fixer.io/api/latest?access_key=${config.API_KEY}`)
+    .then(res => res.json())
+    .then(data => {
+        currencyData = data.rates;
+    })
+    .catch(err => console.log('Error: ', err));
+  }
+
+// This gathers our country data that matched with the search query
 const gatherMatchedCountries = (userInput) => {
   fetch(`https://restcountries.eu/rest/v2/name/${userInput}`)
   .then(res => res.json())
   .then(data => {
-    if (data.status === 404) return false; // No matches found
-    
-    console.log('User Input: ', userInput);
-    console.log(data);
+    if (data.status === 404) {
+        // No matches found
+        document.getElementById('root').innerHTML = 'No Matches Found.';
+        return false;
+    }
     generateDropdown(data);
-
   })
   .catch(err => console.log('Error: ', err));
 }
 
 const generateDropdown = (data) => {
-  // Clears out any potential pre-exisitng values before rendering new ones
   document.getElementById('root').innerHTML = '';
 
   const outerDiv = document.createElement('div');
   outerDiv.setAttribute('class', 'countries-container');
-
 
   data.forEach(country => {
     const innerDiv = document.createElement('div');
@@ -77,78 +77,53 @@ const generateDropdown = (data) => {
     currency.innerText = `Currency: ${country.currencies[0].name} (${country.currencies[0].symbol})`;
     currency.setAttribute('currency-code', country.currencies[0].code);
 
+
+    console.log('1: ', country.currencies[0].code);
+    const conversionData = calculateConversion(country.currencies[0].code);
+    console.log('converstionData : ', conversionData);
+
+    const status = document.createElement('h3');
+
+    if (conversionData >= 1) {
+        // CODE belongs to a weak country
+        status.innerHTML = `Status: <span class="strong-country">Strong</span>`;
+    } else {
+        // CODE belongs to a strong country
+        status.innerHTML = `Status: <span class="weak-country">Weak</span>`;
+    }
+
     innerDiv.appendChild(name);
     innerDiv.appendChild(flag);
     innerDiv.appendChild(currency);
+    innerDiv.appendChild(status);
 
     outerDiv.appendChild(innerDiv);
   });
   document.getElementById('root').appendChild(outerDiv);
 }
 
-const convertCurrency = (e) => {
-  const target = e.target;
-  const currencyCode = target.getAttribute('currency-code');
+const calculateConversion = (currencyCode) => {
+    console.log(currencyCode);
+    console.log(currencyData);
+    // 1 EUR => $x USD
+    const EURtoUSD = currencyData['USD']; 
+    console.log(EURtoUSD);
 
-  if (!currencyCode) return false;
+    // 1 EUR => CODE's Currency
+    const EURtoCODE = currencyData[currencyCode]; 
+    console.log(EURtoCODE);
 
-  console.log (`Currency code is: ${currencyCode}`);
+    // $1USD => CODE's Currency
+    const USDtoCODE = EURtoCODE / EURtoUSD; 
+    console.log(EURtoCODE);
 
-  // console.log('$1 USD => ');
-  // `http://data.fixer.io/api/convert?access_key=${config.API_KEY}&from=USD&to=${currencyCode}&amount=1`
-  fetch(`http://data.fixer.io/api/latest?access_key=${config.API_KEY}`)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-
-    const EURtoUSD = data.rates.USD; // 1 EUR => $x USD
-    const EURtoCODE = data.rates[currencyCode];
-    
-    // So $1 USD = EURtoCODE/EURtoUSD 
-
-    // data.rates
-    // 1 EUR = .434 COL
-    // 1 EUR = 1.231 USD
-
-  })
-  .catch(err => console.log('Error: ', err));
+    return USDtoCODE;
 }
 
-window.addEventListener('mouseover', convertCurrency);
+// Event Listeners
+// Display all countries on initial page load - I want them all rendered
+document.addEventListener('DOMContentLoaded', () => {
+    gatherCurrencyData();
+    gatherAllCountries();
+});
 
-/*
-alpha2Code: "AF"
-alpha3Code: "AFG"
-altSpellings: (2) ["AF", "Afġānistān"]
-area: 652230
-borders: (6) ["IRN", "PAK", "TKM", "UZB", "TJK", "CHN"]
-callingCodes: ["93"]
-capital: "Kabul"
-cioc: "AFG"
-currencies: Array(1)
-0: {code: "AFN", name: "Afghan afghani", symbol: "؋"}
-length: 1
-__proto__: Array(0)
-demonym: "Afghan"
-flag: "https://restcountries.eu/data/afg.svg"
-gini: 27.8
-languages: Array(3)
-0: {iso639_1: "ps", iso639_2: "pus", name: "Pashto", nativeName: "پښتو"}
-1: {iso639_1: "uz", iso639_2: "uzb", name: "Uzbek", nativeName: "Oʻzbek"}
-2: {iso639_1: "tk", iso639_2: "tuk", name: "Turkmen", nativeName: "Türkmen"}
-length: 3
-__proto__: Array(0)
-latlng: (2) [33, 65]
-name: "Afghanistan"
-nativeName: "افغانستان"
-numericCode: "004"
-population: 27657145
-region: "Asia"
-regionalBlocs: [{…}]
-subregion: "Southern Asia"
-timezones: ["UTC+04:30"]
-topLevelDomain: [".af"]
-translations:
-*/
-
-// Best Places to Travel - Where the American Dollar is Best, where the exchange rate is highest
